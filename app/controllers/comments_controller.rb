bf17299bf7fbd685e71693bec9e0f3f45c55707b
gj-ca <glen.johnson@coderacademy.edu.au>
2020-10-23 21:26:45 +1000

    has_many_though

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,8 @@ class CommentsController < ApplicationController
         @comment = Comment.new(
             title: params[:comment][:title], 
             content: params[:comment][:title],
-            blog_id: params[:blog_id]
+            blog_id: params[:blog_id],
+            user_id: params[:comment][:user_id]
         )
         @comment.save!
         redirect_to blog_path(params[:blog_id])
